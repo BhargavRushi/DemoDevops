cec6ae20cba6e71533aa0a2a7a4478cbecc25a39
BhargavRushi <70277043+BhargavRushi@users.noreply.github.com>
2021-09-23 10:33:45 +0530

    DemoProg.py

--- DemoProg.py
+++ DemoProg.py
@@ -1,10 +1,2 @@
-n = int(input("Please enter a number : "))
-count = 0
-for i in range(2,n//2+1):
-    if n%i == 0:
-       count = count + 1
-       break;
-if count == 0:
-   print('Prime')
-else:
-   print('Not Prime')
+a = "Hello World"
+print(a)
